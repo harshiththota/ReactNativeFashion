c76591de8f682d208cf197d9595ddaccf7381add
thota harshith <thotaharshith@thotas-MacBook-Air.local>
2020-08-07 00:21:58 +0530

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module" // Allows for the use of imports
+  },
+  extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
   plugins: ['@typescript-eslint'],
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,5 +2,6 @@ module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
+  tabWidth: 4,
   trailingComma: 'all',
 };
